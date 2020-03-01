f9724bb96eca8773929017ac6a8b486be4c73694
Felipe Lisboa <lisboa@alunos.utfpr.edu.br>
2020-03-01 11:39:34 -0300

    Diff current active window

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -13,12 +13,9 @@ import java.util.*;
 import com.sun.jna.Native;
 
 
-
-
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.platform.win32.WinDef.RECT;
-
 
 
 // getAllWindows
@@ -28,24 +25,30 @@ public class main {
     private static final int MAX_TITLE_LENGTH = 1024;
     SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd hh mm ss a");
 
+    String current = "", past = "";
+
     public void robo()
     {
         try {
             Calendar now = Calendar.getInstance();
-            Robot robot = new Robot();
-            BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            ImageIO.write(screenShot, "JPG", new File("I:\\workflowTest\\" + formatter.format(now.getTime()) + ".jpg"));
-
-
             char[] buffer = new char[MAX_TITLE_LENGTH * 2];
             HWND hwnd = User32.INSTANCE.GetForegroundWindow();
             User32.INSTANCE.GetWindowText(hwnd, buffer, MAX_TITLE_LENGTH);
             System.out.println("Active window title: " + Native.toString(buffer));
-            RECT rect = new RECT();
-            User32.INSTANCE.GetWindowRect(hwnd, rect);
-            System.out.println("rect = " + rect);
+            current = Native.toString(buffer);
 
-            System.out.println(formatter.format(now.getTime()));
+            if(!past.equals(current)){
+                past = Native.toString(buffer);
+                RECT rect = new RECT();
+                User32.INSTANCE.GetWindowRect(hwnd, rect);
+                System.out.println("rect = " + rect);
+                System.out.println(formatter.format(now.getTime()));
+                Robot robot = new Robot();
+                BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                ImageIO.write(screenShot, "JPG", new File("D:\\workflowTest\\" + formatter.format(now.getTime()) + ".jpg"));
+            }
+
+
         }catch(Exception e){
             System.err.println(e);
         }
